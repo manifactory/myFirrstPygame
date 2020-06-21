5aa90f85703a31290ee2814e53879c76929bfa0e
mobrother03 <mobrother03@sunrint.hs.kr>
2020-06-21 23:20:31 +0900

    jump

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pygame, sys
 clock = pygame.time.Clock()
 
 from pygame.locals import *
-
 #pygame 초기화
 pygame.init()
 
@@ -12,50 +11,114 @@ pygame.display.set_caption('My Pygame Window')
 WINDOW_SIZE = (400,400)
 screen = pygame.display.set_mode(WINDOW_SIZE,0,32)
 
-#이미지 리소스
-player_image = pygame.image.load('Sprite-0002.png')
+display = pygame.Surface((200,200))
 
+#이미지 리소스
+player_image = pygame.image.load('Sprite-0001.png')
+
+grass_img = pygame.image.load('grass.png')
+dirt_img = pygame.image.load('dirt.png')
+rock_img = pygame.image.load('rock.png')
 
 #변수 선언
 move_right = False
 move_left = False
 move_up = False
 move_down = False
+press_right = False
+press_left = False
+press_up = False
+press_down = False
 
 player_location = [50,50]
-player_momentum = [0,0.2]
+player_momentum = [0,0]
+
+
+#맵 데이터
+test_map = [[0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0],
+            [1,1,1,1,1,1,1,1,1,1,1,1],
+            [2,2,2,2,2,2,2,2,2,2,2,2],
+            [0,0,0,0,0,0,0,0,0,0,0,0]]
 
 player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
-test_rect = pygame.Rect(100,100,100,50)
+
+def collision_test(rect,tiles):
+    hit_list=[]
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+def check_movement_collide(rect,movement,tiles):
+    collision_type = {'top':False,'bottom':False,'left':False,'right':False}
+    rect.x += movement[0]
+    for tile in collision_test(rect,tiles):
+        if movement[0] < 0:
+            rect.left = tile.left
+            collision_type['left']=True
+        elif movement[0] > 0:
+            rect.left = tile.right
+            collision_type['right']=True
+    rect.y += movement[1]
+    for tile in collision_test(rect,tiles):
+        if movement[1] < 0:
+            rect.top = tile.bottom
+            collision_type['top']=True
+        elif movement[1] > 0:
+            rect.bottom = tile.top
+            collision_type['bottom']=True
+    return rect, collision_type
+
 
 #main code
 while True:
-    screen.fill((255,255,255))
+    display.fill((255,255,255))
 
-    screen.blit(player_image,player_location)
+    #draw tile
+    tile_rect=[]
+    y=0
+    for line in test_map:
+        x=0
+        for tile in line:
+            if tile == 1:
+                display.blit(grass_img,[x*16,y*16])
+            if tile == 2:
+                display.blit(dirt_img,[x*16,y*16])
+            if tile != 0:
+                tile_rect.append(pygame.Rect(x*16,y*16,16,16))
+            x+=1
+        y+=1
+
 
     #bouncing
-    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
+    """if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
         player_momentum[1] = -player_momentum[1]
     else:
-        player_momentum[1] += 0.98
-    player_location[1] += player_momentum[1]
+        player_momentum[1] += 0.98"""
 
     #moving
+    player_movement = [0,0]
     if move_left:
-        player_location[0] -= 4
+        player_movement[0] -= 4
     if move_right:
-        player_location[0] += 4
-
+        player_movement[0] += 4
+    player_movement[1] += player_momentum[1] #중력 흉내
+    player_momentum[1] += 0.98
+    """if player_momentum[1] >= 3:
+        player_momentum[1] = 3"""
     #moving rect
-    player_rect.x=player_location[0]
-    player_rect.y=player_location[1]
+    player_rect, collisions = check_movement_collide(player_rect,player_movement,tile_rect)
+    if collisions['bottom'] == True:
+        player_momentum[1]=0
 
-    #collider 테스트
-    if player_rect.colliderect(test_rect):
-        pygame.draw.rect(screen,(255,0,0),test_rect)
-    else:
-        pygame.draw.rect(screen,(0,0,0),test_rect)
+    for tile in tile_rect:
+        pygame.draw.rect(display,(255,0,0),tile)
+    display.blit(player_image,[player_rect.x,player_rect.y])
 
     #event loop
     for event in pygame.event.get():
@@ -70,6 +133,7 @@ while True:
             if event.key == K_RIGHT:
                 move_right=True
             if event.key == K_UP:
+                player_momentum[1] = -10
                 move_up=True
             if event.key == K_DOWN:
                 move_down=True
@@ -84,8 +148,6 @@ while True:
             if event.key == K_DOWN:
                 move_down=False
 
+    screen.blit(pygame.transform.scale(display,WINDOW_SIZE),[0,0])
     pygame.display.update()
     clock.tick(60)
-
-pygame.quit()
-sys.exit()
